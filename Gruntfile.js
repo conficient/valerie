f6c3e8c985db3c327a438d12d502578aaf3bc5ee
Dave Watts <dave@egrove.co.uk>
2013-07-09 10:50:50 +0100

    Fixed apidocs tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
             ]
         },
         "compress": {
-            "apiDocs": {
+            "apidocs": {
                 "options": {
                     "archive": "apiDocs.zip",
                     "mode": "zip"
