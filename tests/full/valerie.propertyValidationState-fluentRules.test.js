18b409ee465885546e7584a2fa971d3e5ba1b9ef
Dave Watts <dave@egrove.co.uk>
2013-07-17 17:16:08 +0100

    Tests for setting converters fluently. #62

---
+++ tests/full/valerie.propertyValidationState-fluentRules.test.js
@@ -0,0 +1,65 @@
+describe("PropertyValidationState - Fluent Converters", function () {
+    describe("currencyMajor", function () {
+        it("should change the property validation state to use the currencyMajor converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).currencyMajor();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.currencyMajor);
+        });
+    });
+
+    describe("currencyMajorMinor", function () {
+        it("should change the property validation state to use the currencyMajorMinor converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).currencyMajorMinor();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.currencyMajorMinor);
+        });
+    });
+
+    describe("date", function () {
+        it("should change the property validation state to use the date converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).date();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.date);
+        });
+    });
+
+    describe("email", function () {
+        it("should change the property validation state to use the email converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).email();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.email);
+        });
+    });
+
+    describe("float", function () {
+        it("should change the property validation state to use the float converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).float();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters["float"]);
+        });
+    });
+
+    describe("integer", function () {
+        it("should change the property validation state to use the integer converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).integer();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.integer);
+        });
+    });
+
+    describe("number", function () {
+        it("should change the property validation state to use the number converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).number();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.number);
+        });
+    });
+
+    describe("string", function () {
+        it("should change the property validation state to use the passThrough converter", function () {
+            var propertyValidationState = new valerie.PropertyValidationState(ko.observable()).string();
+
+            expect(propertyValidationState.settings.converter).toEqual(valerie.converters.passThrough);
+        });
+    });
+})
